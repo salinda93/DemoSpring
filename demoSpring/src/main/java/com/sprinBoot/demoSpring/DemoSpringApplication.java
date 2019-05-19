ca6111fc452cee60c837320fe40111e0fdde6efe
Salinda <salinda.jayasundara3010@gmail,com>
2019-05-19 13:52:24 +0530

    User Management Profile Crud

--- demoSpring/src/main/java/com/sprinBoot/demoSpring/DemoSpringApplication.java
+++ demoSpring/src/main/java/com/sprinBoot/demoSpring/DemoSpringApplication.java
@@ -1,6 +1,7 @@
 package com.sprinBoot.demoSpring;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
